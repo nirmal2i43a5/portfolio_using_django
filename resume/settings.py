2952cae8670fe2f028085e4c1575eb0f0af64071
Nirmal <nirmalpandey048@gmail.com>
2020-09-08 15:17:54 +0545

    Updated

--- resume/settings.py
+++ resume/settings.py
@@ -123,9 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT =  '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 
