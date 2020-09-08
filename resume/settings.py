f5d671f47d5607074ecc09354ceee41881562946
Nirmal <nirmalpandey048@gmail.com>
2020-09-08 15:19:04 +0545

    Updated

--- resume/settings.py
+++ resume/settings.py
@@ -134,7 +134,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)#for global static i.e for deployment
+# STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)#for global static i.e for deployment
 
 
 
