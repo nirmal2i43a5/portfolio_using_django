1bfc1863b96f689275c5cf45ba89759d45553a0f
Nirmal <nirmalpandey048@gmail.com>
2020-09-08 10:28:04 +0545

    Updated

--- resume/urls.py
+++ resume/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('resume_app.urls',namespace = "resume_app"))
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
