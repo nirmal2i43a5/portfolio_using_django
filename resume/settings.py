177737a458364b5f8e9f87e0d643b9393d82cbf8
Nirmal <nirmalpandey048@gmail.com>
2020-09-08 15:38:06 +0545

    Updated

--- resume/settings.py
+++ resume/settings.py
@@ -128,11 +128,11 @@ STATIC_URL = '/static/'
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'portfolio/static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR,'portfolio/media/')
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)#for global static i.e for deployment
 
