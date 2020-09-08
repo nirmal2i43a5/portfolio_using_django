88d6ebad6449430b2bdd1e52f7e7b8ba91f94b4e
Nirmal <nirmalpandey048@gmail.com>
2020-09-08 08:59:22 +0545

    Updated

--- resume/settings.py
+++ resume/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = '/home/nirmal/Documents/Django Project Inventory/django/portfolio/static/images'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
