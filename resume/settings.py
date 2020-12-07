ecb05905ff231772c1ce7df0f6b921481c47fd94
Nirmal <nirmalpandey048@gmail.com>
2020-12-07 18:14:35 +0545

    Updated

--- resume/settings.py
+++ resume/settings.py
@@ -135,8 +135,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#for heroku staticfiles
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
